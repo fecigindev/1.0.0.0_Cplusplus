72423ccdb7e59ad087cf4b296442785d65b46e7d
fecigindev <33804459+fecigindev@users.noreply.github.com>
2017-11-21 00:06:10 -0800

    Update 1.0.0.1_Nama pengenal.cpp

--- 1.0.0.1_Nama pengenal.cpp
+++ 1.0.0.1_Nama pengenal.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+
+int main()
+{
+    int benar;
+    int benar89;
+    int harga_naik;
+
+    int 9benar;
+    int ini~salah;
+    int harga..naik;
+}
